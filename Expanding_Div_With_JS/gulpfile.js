ac1615b57a3933f1653889eadf4791ea245748b5
Aimee <aimee.tacchi@octoenergy.com>
2017-04-01 22:56:51 +0100

    add new gulp task, fileinclude. which allows you to include html files into html files

--- Expanding_Div_With_JS/gulpfile.js
+++ Expanding_Div_With_JS/gulpfile.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var gulp = require('gulp'),
-  concat = require('gulp-concat'),
+  //concat = require('gulp-concat'),
 	sass = require('gulp-sass'),
-	browserSync = require('browser-sync').create();
+	browserSync = require('browser-sync').create(),
+	fileinclude = require('gulp-file-include'),
+	rename = require("gulp-rename"),
+	path = require("path");
 
 /*gulp.task("concatScripts", function() {
 	gulp.src([
@@ -14,6 +17,7 @@ var gulp = require('gulp'),
 	.pipe(gulp.dest("js"));
 });*/
 
+// 4.
 gulp.task('compileSass', function() {
 	return gulp.src("scss/application.scss")
 		.pipe(sass())
@@ -21,11 +25,32 @@ gulp.task('compileSass', function() {
 		.pipe(browserSync.stream());
 });
 
+var paths = {
+	templates: './templates/'
+};
+
+//run: gulp fileinclude
+//including HTML FILES INTO HTML FILES TO SAVE REPEATING YOURSELF.
+gulp.task('fileinclude', function() {
+	// this establishes where the file is you want gulp to get.
+  return gulp.src(path.join(paths.templates, '*.tpl.html'))
+    .pipe(fileinclude())
+    .pipe(rename({
+    	extname: ""
+    }))
+    .pipe(rename({
+    	extname: ".html"
+    }))
+    .pipe(gulp.dest('./'));
+});
+// 3.
 gulp.task("build", ['compileSass'], function(){
 	return gulp.src(["css/appliation.css", "js/app.js"], { base: './'})
 	.pipe(gulp.dest('dist'));
 });
 
+// this task is run when you run gulp, as it's called in the default task, which then calls and runs 'build' above, then that calls and runs 'complileSass' which is run first.
+// 2.
 gulp.task('browser-sync', ['build'], function() {
     browserSync.init({
         server: {
@@ -33,11 +58,13 @@ gulp.task('browser-sync', ['build'], function() {
         }
     });
 
+
 	gulp.watch('scss/*.scss', ['compileSass']);
 	gulp.watch('js/*.js', ['build']);
 	gulp.watch('*.html').on('change', browserSync.reload);
 });
 
+// 1. run gulp
 gulp.task("default", ['browser-sync']);
 
 
